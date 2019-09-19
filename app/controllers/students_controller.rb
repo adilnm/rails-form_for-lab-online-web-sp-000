2f67d875d05ea7d203b2b3d1d1f27273ae5260de
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-19 19:28:26 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,5 +20,10 @@ class StudentsController < ApplicationController
   def edit
     @student=Student.find(params[:id])
   end
-  
+
+  def update
+    @student=Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
 end
