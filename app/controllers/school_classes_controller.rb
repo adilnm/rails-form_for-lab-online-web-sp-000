5af2d9acfa1babf033aad39a2e510374fdcbcc04
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-19 18:53:00 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,4 +7,14 @@ end
 def show
   @school_class=SchoolClass.find(params[:id])
 end
+
+def new
+  @school_class=SchoolClass.new
+end
+
+def create
+  @student=Student.new(params.require(:student).permit(:first_name, :last_name))
+  @student.save
+  redirect_to student_path(@student)
+end
 end
