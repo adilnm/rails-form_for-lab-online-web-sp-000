f415b573a8882ee7096dba9408909f84fdec0f17
ADIL NAIMI <adilnm23@hotmail.com>
2019-09-19 18:50:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student=Student.new(params.require(:student).permit(:first_name))
+    binding.pry
+    @student=Student.new(params.require(:student).permit(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
